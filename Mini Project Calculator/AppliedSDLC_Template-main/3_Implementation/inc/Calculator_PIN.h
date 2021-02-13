ecbe99fdbe229ec044f8698bc1dd4b880a9381e0
99003730 <78853960+99003730@users.noreply.github.com>
2021-02-13 17:32:10 +0530

    add definition and functions for PIN

---
+++ Mini Project Calculator/AppliedSDLC_Template-main/3_Implementation/inc/Calculator_PIN.h
@@ -0,0 +1,11 @@
+#ifndef __CALCULATOR_PIN_H__
+#define __CALCULATOR_PIN_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int check_pin(int pin);
+
+int update_pin(int new_pin);
+#endif

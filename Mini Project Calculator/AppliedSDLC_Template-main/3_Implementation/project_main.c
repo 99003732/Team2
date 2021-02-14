90081d53ab8b21132dc25a3ed063c94a1c3811be
99003730 <78853960+99003730@users.noreply.github.com>
2021-02-14 19:43:08 +0530

    Arnav Add comments to file

--- Mini Project Calculator/AppliedSDLC_Template-main/3_Implementation/project_main.c
+++ Mini Project Calculator/AppliedSDLC_Template-main/3_Implementation/project_main.c
@@ -26,9 +26,9 @@ int valid_operation(int operation);
 
 /* Start of the application */
 int main(int argc, char *argv[])
-{int last_pin;
-  int entered_pin;
-  last_pin=get_pin();
+{int last_pin;//Input for previous pin
+  int entered_pin;//Input for user entered pin
+  last_pin=get_pin();//checking with the pin saved on file pin.txt
 printf("Enter PIN: ");
 scanf("%d",&entered_pin);
     if(correct_pin(entered_pin)==1)

9c64c8103937a7349351ee3a33927b0c2176e4bd
99003729 <pushkar.antony@ltts.com>
2021-02-15 10:19:05 +0530

    Commiting the changes in operations

--- Mini Project Calculator/AppliedSDLC_Template-main/3_Implementation/project_main.c
+++ Mini Project Calculator/AppliedSDLC_Template-main/3_Implementation/project_main.c
@@ -62,9 +62,9 @@ void calculator_menu(void)
     }
     else if(ADD == calculator_operation)
     {
-        printf("\n\tEnter the Number of operands you want to add \n");
+        printf("\n\tEnter your Numbers with space between them\n");
         //__fpurge(stdin);
-        scanf("%d ",no_of_op);
+        scanf("%d %d", &calculator_operand1, &calculator_operand2);
     }
     else if(SUBTRACT == calculator_operation)
     {
@@ -119,10 +119,12 @@ void calculator_menu(void)
     switch(calculator_operation)
     {
         case ADD:
-            printf(" %d\n", 
-            add(no_of_op));
+            printf("\n\t%d + %d = %d\nEnter to continue", 
+            calculator_operand1, 
+            calculator_operand2,
+            add(calculator_operand1, calculator_operand2));
             
-           // __fpurge(stdin);
+            // __fpurge(stdin);
             getchar();
             break;
         case SUBTRACT:

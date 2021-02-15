a7e3d6548a320b932aa9e47a39e9864a756de338
99003728 <78848803+99003728@users.noreply.github.com>
2021-02-15 11:41:08 +0530

    Update test_calculator_operations.c

--- Mini Project Calculator/AppliedSDLC_Template-main/3_Implementation/test/test_calculator_operations.c
+++ Mini Project Calculator/AppliedSDLC_Template-main/3_Implementation/test/test_calculator_operations.c
@@ -42,29 +42,34 @@ int main()
 void test_add(void) {
   TEST_ASSERT_EQUAL(30, add(10, 20));
   TEST_ASSERT_EQUAL(-10, add(10, -20));
+  TEST_ASSERT_EQUAL(-100, add(10, -110));
 }
 void test_add_testcase2(void) {
  
   /* Dummy fail*/
   TEST_ASSERT_EQUAL(1500, add(750, 7500));
+  
+  TEST_ASSERT_EQUAL(100, add(250, 750));
 }
 void test_subtract(void) {
   TEST_ASSERT_EQUAL(-3, subtract(0, 3));
+  TEST_ASSERT_EQUAL(3, subtract(6, 3));
   
   /* Dummy fail*/
   TEST_ASSERT_EQUAL(1, subtract(1000, 900));
+  TEST_ASSERT_EQUAL(10, subtract(1000, 90));
 }
 
 void test_multiply(void) {
   TEST_ASSERT_EQUAL(0, multiply(1, 0));
-  
+  TEST_ASSERT_EQUAL(10, multiply(1, 10))
   /* Dummy fail*/
   TEST_ASSERT_EQUAL(2, multiply(2, 5));
 }
 
 void test_divide(void) {
   TEST_ASSERT_EQUAL(0, divide(1, 0));
-  
+  TEST_ASSERT_EQUAL(3, divide(6, 2));
   /* Dummy fail*/
   TEST_ASSERT_EQUAL(3, divide(2, 2));
 }
